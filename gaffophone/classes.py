500a473920ef9632c7163511e0e22ca0844933f0
Denis Moreno <denis.moreno@scilifelab.se>
2017-05-22 13:53:11 +0200

    implement templating system

--- gaffophone/classes.py
+++ gaffophone/classes.py
@@ -1,22 +1,34 @@
 import praw
 import datetime
+import codecs
+
 
 class Gaffophone():
-    def __init__(self):
+    def __init__(self, args):
         self.reddit = praw.Reddit('gaffophone')
         self.france =  self.reddit.subreddit("france")
         self.rance =  self.reddit.subreddit("rance")
         self.test = self.reddit.subreddit("testingground4bots")
+        self.body_template = args.body_template
+        self.title_template = args.title_template
+        self.submission_template = args.submission_template
 
     def run(self):
-        text=[]
-        title="Le meilleur de /r/rance, semaine {}".format(datetime.datetime.now().strftime("%V"))
+        submissions=[]
+        with codecs.open(self.title_template, 'r', 'utf-8') as tt:
+            title = tt.read().encode('utf-8').format(weeknumber=datetime.datetime.now().strftime("%V"))
+
+        with codecs.open(self.body_template, 'r', 'utf-8') as bt:
+            body = bt.read().encode('utf-8')
+
+        with codecs.open(self.submission_template, 'r', 'utf-8') as st:
+            sub_temp = st.read().encode('utf-8')
         for submission in self.rance.top('week', limit=10):
+            submissions.append(sub_temp.format(submission_title=submission.title.encode('utf-8'), 
+                                               submission_url=submission.url.encode('utf-8'), user=submission.author,
+                                               comments_url=submission.permalink.encode('utf-8')))
 
-            text.append(u'[{}]({}) par /u/{}\n'.format(submission.title, submission.url, submission.author).encode('utf-8'))
-            print submission.title
-            print submission.url
+        post = body.format(submissions="\n".join(submissions))
 
-
-        self.test.submit(title, selftext="\n".join(text))
+        self.test.submit(title, selftext=post)
 
--- scripts/run.py
+++ scripts/run.py
@@ -1,13 +1,19 @@
 
 from gaffophone.classes import Gaffophone
+import argparse
 
 
-def main():
-    gf = Gaffophone()
+def main(args):
+    gf = Gaffophone(args)
     gf.run()
 
 
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", default="body.mkd", dest="body_template")
+    parser.add_argument("-t", default="title.mkd", dest="title_template")
+    parser.add_argument("-s", default="submission.mkd", dest="submission_template")
+    args = parser.parse_args()
+    main(args)
